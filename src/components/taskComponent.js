69b06901dfa2db400a6dc54abb17d3913aa5558b
James White <whitejamesthe2nd@gmail.com>
2021-06-17 23:19:18 -0400

    yup

--- src/components/taskComponent.js
+++ src/components/taskComponent.js
@@ -9,6 +9,7 @@ const TaskComponent = ()=>{
         <div>
 
             <input type={'checkbox'}></input>
+            <input type={'textArea'}></input>
             <p>Words</p>
         </div>
     )
